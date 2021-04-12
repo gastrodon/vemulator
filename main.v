9c693d13db372aeb1bb7980ba688f29ec247ec24
Eva Harris <mail@gastrodon.io>
2021-04-11 20:06:23 -0400

    Split up registers
    Split up 16 bit register pairs into individual byte registers for easier
    solo math
    
    Add some early opcodes dealing mostly with bc / b registers
    
    Rework the print layout of the machine state

--- main.v
+++ main.v
@@ -14,9 +14,12 @@ mut:
 	flags byte = 0b00000010
 
 	a  byte
-	bc u16
-	de u16
-	hl u16 // Also m
+	b  byte
+	c  byte
+	d  byte
+	e  byte
+	h  byte
+	l  byte
 	sp u16
 	pc u16
 }
@@ -35,8 +38,23 @@ fn (mut state State) execute(rom []byte) {
 			0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38 {}
 			// LXI B d16
 			0x01 {
-				state.bc = u16(rom[pc + 2]) << 8 | u16(rom[pc + 1])
+				state.b = rom[pc + 2]
+				state.c = rom[pc + 1]
 				pc += 2
+			}
+			// STAX B
+			0x02 {
+				state.b = state.a >> 8
+				state.c = state.a & 0xFF
+			}
+			// INX B
+			0x03 {
+				// TODO flags
+				state.b++
+			}
+			0x04 {
+				// TODO flags
+				state.b--
 			}
 			// STA a16
 			0x32 {
@@ -63,12 +81,15 @@ fn (mut state State) execute(rom []byte) {
 }
 
 fn (state State) print() {
-	print('a  $state.a ')
-	print('bc $state.bc ')
-	print('de $state.de \n')
-	print('hl $state.hl ')
-	print('sp $state.sp ')
+	print('a  $state.a\t')
+	print('sp $state.sp\t')
 	print('pc $state.pc \n')
+	print('b  $state.b\t')
+	print('c  $state.c\t')
+	print('d  $state.d \n')
+	print('e  $state.e\t')
+	print('h  $state.h\t')
+	print('l  $state.l \n')
 
 	print('\nmem  | ')
 
